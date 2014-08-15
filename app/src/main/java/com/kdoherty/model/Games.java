cdf05c9706c3c762f3db41c0fbbe1080dd5cb4db
kdoherty11 <kdoherty@localhost.localdomain>
2014-08-14 22:40:45 -0400

    Adding Games for deserialization

---
+++ app/src/main/java/com/kdoherty/model/Games.java
@@ -0,0 +1,15 @@
+package com.kdoherty.model;
+
+import java.util.List;
+
+/**
+ * Created by kdoherty on 8/14/14.
+ */
+public class Games {
+
+    private List<Game> games;
+
+    public List<Game> getGames() {
+        return games;
+    }
+}
