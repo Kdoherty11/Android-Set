8656f222506a23219b62af1070875c7968e77f16
kdoherty11 <kdoherty@localhost.localdomain>
2014-08-24 23:53:20 -0400

    Made Player Comparable and Parcelable

--- app/src/main/java/com/kdoherty/set/model/Player.java
+++ app/src/main/java/com/kdoherty/set/model/Player.java
@@ -1,9 +1,12 @@
 package com.kdoherty.set.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by kdoherty on 8/6/14.
  */
-public class Player {
+public class Player implements Comparable<Player>, Parcelable {
 
     private String name;
     private int score;
@@ -11,6 +14,10 @@ public class Player {
     public Player(String name, String regId, int score) {
         this.name = name;
         this.score = score;
+    }
+
+    public Player(Parcel in) {
+        readFromParcel(in);
     }
 
     public String getName() {
@@ -24,4 +31,37 @@ public class Player {
     public String toString() {
         return "Name " + name + " Set Count: " + score;
     }
+
+    @Override
+    public int compareTo(Player player) {
+        return player.score - score;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(name);
+        out.writeInt(score);
+    }
+
+    private void readFromParcel(Parcel in) {
+        name = in.readString();
+        score = in.readInt();
+    }
+
+    public static final Creator<Player> CREATOR = new Creator<Player>() {
+
+        public Player createFromParcel(Parcel in) {
+            return new Player(in);
+        }
+
+        public Player[] newArray(int size) {
+            return new Player[size];
+        }
+
+    };
 }
