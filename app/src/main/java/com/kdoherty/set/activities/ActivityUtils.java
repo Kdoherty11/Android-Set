8acd72babf6c6f5ef24c2209eafb8e2410b68be3
kdoherty11 <kdoherty@localhost.localdomain>
2014-08-30 19:34:10 -0400

    Added utils method for checking internet connectivity

--- app/src/main/java/com/kdoherty/set/activities/ActivityUtils.java
+++ app/src/main/java/com/kdoherty/set/activities/ActivityUtils.java
@@ -1,8 +1,10 @@
 package com.kdoherty.set.activities;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
 
 import com.google.common.base.Strings;
 import com.kdoherty.set.Constants;
@@ -18,16 +20,38 @@ public class ActivityUtils {
 
     private static String mUsername;
 
-    public static String getUsername(Activity activity) {
+    public static String getUsername(Context context) {
         if (!Strings.isNullOrEmpty(mUsername)) {
             return mUsername;
         }
-        SharedPreferences usernamePrefs = activity.getSharedPreferences(Constants.Keys.SPF_USERNAME, Context.MODE_PRIVATE);
+        SharedPreferences usernamePrefs = context.getSharedPreferences(Constants.Keys.SPF_USERNAME, Context.MODE_PRIVATE);
         if (usernamePrefs.contains(Constants.Keys.USERNAME)) {
             mUsername = usernamePrefs.getString(Constants.Keys.USERNAME, "Default");
             return mUsername;
         }
         throw new IllegalStateException("Must be logged in");
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkOnline(Context context) {
+        return checkOnline(context, "Internet connectivity is required");
+    }
+
+    public static boolean checkOnline(Context context, String message) {
+        boolean isOnline = isOnline(context);
+        if (!isOnline) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+        return isOnline;
     }
 
     public static int getDifficulty(String option) {
