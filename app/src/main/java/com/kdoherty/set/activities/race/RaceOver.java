006ccd527b0b4631549968379c4023e22ff320ce
kdoherty11 <kdoherty@localhost.localdomain>
2014-08-30 00:16:24 -0400

    Keeping track of high scores and sending them to a leaderboard

--- app/src/main/java/com/kdoherty/set/activities/race/RaceOver.java
+++ app/src/main/java/com/kdoherty/set/activities/race/RaceOver.java
@@ -1,7 +1,9 @@
 package com.kdoherty.set.activities.race;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,9 +12,18 @@ import android.widget.TextView;
 
 import com.kdoherty.set.Constants;
 import com.kdoherty.set.R;
+import com.kdoherty.set.activities.ActivityUtils;
 import com.kdoherty.set.activities.HomeScreen;
+import com.kdoherty.set.activities.Leaderboard;
+import com.kdoherty.set.services.SetApi;
 
-public class RaceOver extends Activity {
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class RaceOver extends Activity implements View.OnClickListener {
+
+    private int mTarget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +34,34 @@ public class RaceOver extends Activity {
         TextView timeView = (TextView) findViewById(R.id.time);
         timeView.setText("Time: " + time);
 
-        int targetNum = extras.getInt(Constants.Keys.TARGET);
+        mTarget = extras.getInt(Constants.Keys.TARGET);
         TextView target = (TextView) findViewById(R.id.target);
-        target.setText("Target: " + targetNum);
+        target.setText("Target: " + mTarget);
+
+        SharedPreferences prefs = getSharedPreferences(Constants.Keys.SPF_HIGHSCORE, Context.MODE_PRIVATE);
+        long highScore = prefs.getLong(Constants.Keys.RACE_HIGH_SCORE + String.valueOf(mTarget), Long.MAX_VALUE);
+        long elapsedTime = extras.getLong(Constants.Keys.ELAPSED_TIME_RACE);
+        if (elapsedTime < highScore) {
+            SharedPreferences.Editor edit = prefs.edit();
+            edit.putLong(Constants.Keys.RACE_HIGH_SCORE + String.valueOf(mTarget), elapsedTime);
+            edit.commit();
+            ((TextView) findViewById(R.id.gameOver)).setText("HIGH SCORE");
+            SetApi.INSTANCE.insertRaceEntry(mTarget, ActivityUtils.getUsername(this), elapsedTime, new Callback<Response>() {
+                @Override
+                public void success(Response response, Response response2) {
+                    System.out.println("success submitting race score");
+                }
+
+                @Override
+                public void failure(RetrofitError error) {
+                    System.out.println("failure submitting race score");
+                }
+            });
+        }
+
+        findViewById(R.id.restart).setOnClickListener(this);
+        findViewById(R.id.home).setOnClickListener(this);
+        findViewById(R.id.leaderboard).setOnClickListener(this);
     }
 
     @Override
@@ -47,13 +83,26 @@ public class RaceOver extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void restart(View v) {
-        Intent times = new Intent(getApplicationContext(), RaceSetUp.class);
-        startActivity(times);
-    }
-
-    public void home(View v) {
-        Intent home = new Intent(getApplicationContext(), HomeScreen.class);
-        startActivity(home);
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        switch(id) {
+            case R.id.restart:
+                Intent times = new Intent(getApplicationContext(), RaceSetUp.class);
+                startActivity(times);
+                break;
+            case R.id.home:
+                Intent home = new Intent(getApplicationContext(), HomeScreen.class);
+                startActivity(home);
+                break;
+            case R.id.leaderboard:
+                Intent leaderboard = new Intent(getApplicationContext(), Leaderboard.class);
+                leaderboard.putExtra(Constants.Keys.TARGET, mTarget);
+                leaderboard.putExtra(Constants.Keys.GAME_MODE, Constants.Modes.RACE);
+                startActivity(leaderboard);
+                break;
+            default:
+                throw new IllegalStateException("Should not get to default case");
+        }
     }
 }
